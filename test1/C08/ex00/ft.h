f6b05be58b80087b11d27b0819ede5c67a442fcc
Sangwon Kim <sakim@c8r7s9.42seoul.kr>
2020-12-10 01:34:33 +0900

    good

--- test1/C08/ex00/ft.h
+++ test1/C08/ex00/ft.h
@@ -1,20 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*   ft.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sakim <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: sakim </var/mail/sakim>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/24 22:16:26 by sakim             #+#    #+#             */
-/*   Updated: 2020/12/10 01:15:59 by sakim            ###   ########.fr       */
+/*   Created: 2020/12/10 01:10:30 by sakim             #+#    #+#             */
+/*   Updated: 2020/12/10 01:11:25 by sakim            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	ft_swap(int *a, int *b)
-{
-	int temp;
+void ft_putchar(char c);
+void ft_swap(int *a, int *b);
+void ft_putstr(char *str);
+int ft_strlen(char *str);
+int ft_strcmp(char *s1, char *s2);
 
-	temp = *b;
-	*b = *a;
-	*a = temp;
-}
