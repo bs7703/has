c5644f54b52e3a46c53dc9a8e8218415fb5a5eff
sangwon <sangwon@gmail.com>
2020-12-10 04:33:40 +0900

    good

--- test1/C03/ex01/ft_strncmp.c
+++ test1/C03/ex01/ft_strncmp.c
@@ -12,7 +12,7 @@
 
 int	ft_strncmp(char *s1, char *s2, unsigned int n)
 {
-	while (n-- > 0 && *s1 && (*(s1) == *(s2)) && (*s1++ && *s2++))
+	while (*s1 && (*(s1) == *(s2)) && (n-- > 1) && (*(s1++) && *(s2++))
 	{
 	}
 	return ((char)((unsigned char)*s1 - (unsigned char)*s2));
